461694df538519a892fa9935b954c6a34c242f5e
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2025-12-30 10:23:56 +0000

    Update CourseraTest.cs

--- .NET_in_Backend/CourseraTest.cs
+++ .NET_in_Backend/CourseraTest.cs
@@ -1,1 +1,65 @@
 
+using System.Text.Json.Serialization;
+using System.Xml;
+using Newtonsoft.Json;
+public class Product
+{
+    public string name {get ; set; }
+    public float price { get; set; }
+    public List<string> tags {get; set; }
+}
+
+public class Program
+{
+    
+    public static void Main()
+    {
+        /*
+        string json = "{\"name\":\"Laptop\", \"price\":99.99, \"tags\": [\"Electronics\",\"Computers\"]}";
+        Product product = JsonConvert.DeserializeObject<Product>(json);
+        Console.WriteLine($"Product: {product.name}, Price: {product.price}, tags: {string.Join(", ", product.tags)}");
+        */
+
+        Product newproduct = new Product
+        {
+            name = "SmartPhone",
+            price = 966.33,
+            tags = new List<string> {"Electronics", "Mobile"},
+        };
+
+        //We use try/catch to throw out an exceptio if needed
+        try
+        {
+            string newJson = JsonConvert.SerializeObject(newproduct, Formatting.Indented);
+            Console.WriteLine($"Serialised json:\n{newJson}");
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine("Serialization failed:");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+
+
+/*
+using Newtonsoft.Json;
+
+public class Person{
+    public required string name {get; set; }
+    public required int age {get; set; }
+    public required List<string> nationality {get; set;}
+}
+
+public class Program{
+    static void Main(){
+        Person person = new Person{
+            name = "Thomas",
+            age = 30,
+            nationality = new List<string>{"British", "French"},
+        };
+        string newJson = JsonConvert.SerializeObject(person, Formatting.Indented);
+        Console.WriteLine($"Serialised: \n {newJson}");
+    }
+}*/
